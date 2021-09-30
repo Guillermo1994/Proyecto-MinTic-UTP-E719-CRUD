38761f2237780084058bb4d701d39260a70e1522
Guillermo Ramirez <garamirez1994@gmail.com>
2021-09-29 21:15:17 -0500

    Primera Version del proyector CRUD

---
+++ src/routes/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const Task= require('../models/task')
+
+//Leer desde la base de datos
+router.get('/', async (req, res)=> {
+    const tasks = await Task.find();
+    //console.log(tasks);
+    res.render('index', {
+    tasks //tasks: tasks
+    });
+})
+
+//Guardar en base de datos
+router.post('/add', async (req, res)=> { //Recibiendo los datos que vienen desde el formulario
+const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
+    //res.send('received');
+})
+
+//Boton Done
+router.get('/turn/:id', async (req, res) => {
+    const {id} = req.params;
+    const task= await Task.findById(id);
+    task.status = !task.status;
+    res.redirect('/');
+    await task.save();
+    //console.log(task);
+    //res.send('Receidved!')
+});
+
+//Editar
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const task= await Task.findById(id);
+    res.render('edit', {
+    task
+    });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    await Task.updateOne({_id: id}, req.body);
+    res.redirect('/');
+})
+//Borrar
+router.get('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    await Task.remove({_id: id});
+    res.redirect('/');
+    //console.log(req.params.id)
+    //res.send('received!');
+});
+
+module.exports = router; // permite exportar para que se lea desde otro lugar
